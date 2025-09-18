e26ddce8183661427ee5c6b7569b1821921b10a0
vipulkanade <kanade.vipul@gmail.com>
2025-09-17 22:53:42 -0700

    Add high score tracking and continuous gameplay

--- scoreboard.py
+++ scoreboard.py
@@ -2,12 +2,15 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "bold")
+HIGH_SCORE_FILE = "highscore.txt"
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("highscore.txt", "r") as highscore_file:
+            self.high_score = int(highscore_file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,7 +19,15 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore.txt", "w") as highscore_file:
+                highscore_file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0,0)
